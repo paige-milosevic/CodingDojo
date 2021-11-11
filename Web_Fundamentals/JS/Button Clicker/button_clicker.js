883d1cade24242212a47cf022146e03fe8b28d90
paige-milosevic <87679521+paige-milosevic@users.noreply.github.com>
2021-11-10 21:05:23 -0700

    v 11.10.21.1

---
+++ Web_Fundamentals/JS/Button Clicker/button_clicker.js
@@ -0,0 +1,9 @@
+function logout(element) {
+    element.innerText = "Logout";
+    console.log("Hello");
+}
+
+function removeDefinition(element) {
+    element.remove();
+    console.log("Paige");
+}
---
+++ Web_Fundamentals/JS/Fizzbuzz/fizzbuzz.js
@@ -0,0 +1,15 @@
+// console.log the "FIzzBuzz when a number is a multiple of 20"/
+
+var groupOfNums = [123, 60];
+
+for(let i = 0; i<groupOfNums.length; i++) {
+    if (groupOfNums[i] % 20 == 0) {
+        console.log("FizzBuzz", groupOfNums[i])
+    }
+    else if (groupOfNums[i] % 20 == 0) {
+        
+    }
+
+
+
+}
